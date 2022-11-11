7e766985556498cc72c22c4b085cd3bec4c101ce
aira1001 <adindakhaerani95@gmail.com>
2022-11-11 19:34:01 +0700

    edit index

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 <p align="center">
 <font size="8">
 <b>LED 1 = &nbsp <a
-href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field1=1">ON
+href="https://api.thingspeak.com/update?api_key=L36ISF3W2SJ6IX2K&field1=1">ON
 </a></b> / <b><a
-href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field1=0">OFF</a><
+href="https://api.thingspeak.com/update?api_key=L36ISF3W2SJ6IX2K&field1=0">OFF</a><
 /
 b> <br>
-<b>LED 2 = &nbsp <a href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field2=1">ON </a></b> /
+<b>LED 2 = &nbsp <a href="https://api.thingspeak.com/update?api_key=L36ISF3W2SJ6IX2K&field2=1">ON </a></b> /
 <b><a
-href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field2=0">OFF</a><
+href="https://api.thingspeak.com/update?api_key=L36ISF3W2SJ6IX2K&field2=0">OFF</a><
 /
 b> <br>
 </font>
