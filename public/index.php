400afca3e2095d765f72c150c2a7986d14f0cb76
aira1001 <adindakhaerani95@gmail.com>
2022-11-11 18:37:19 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>LED ON/OFF</title>
+</head>
+
+<body>
+<p align="center">
+<font size="8">
+<b>LED 1 = &nbsp <a
+href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field1=1">ON
+</a></b> / <b><a
+href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field1=0">OFF</a><
+/
+b> <br>
+<b>LED 2 = &nbsp <a href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field2=1">ON </a></b> /
+<b><a
+href="https://api.thingspeak.com/update?api_key=XU5AAPTZU6WART0V&field2=0">OFF</a><
+/
+b> <br>
+</font>
+</p>
+</body>
+</html>
